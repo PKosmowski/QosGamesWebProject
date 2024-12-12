b496c637a37867181743fd1eb7ce550fee9b68e3
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-12 14:39:52 +0100

    added AboutUs.tsx

--- website/src/components/Nav-components/AboutUs.tsx
+++ website/src/components/Nav-components/AboutUs.tsx
@@ -1,4 +1,4 @@
-
+import './AboutUs.css'
 
 
 export default function AboutUs () {
@@ -9,12 +9,14 @@ export default function AboutUs () {
                     <div className="header">Qos Games</div>
                     <div className="desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, quidem aliquid odit omnis qui, facere laboriosam provident similique et molestiae magnam ex nisi voluptatibus? Deserunt eum distinctio culpa atque quas!</div>
                 </div>
-                <div className="logo"></div>
+                <div className="logo">
+                    <img src="website\src\assets\react.svg" alt="" />
+                </div>
             </div>
-            <div className="container-1">
+            <div className="container-2">
                 <div className="text">
-                    <div className="header"></div>
-                    <div className="desc"></div>
+                    <div className="header">Latest news</div>
+                    <div className="desc">Witam to sa drugie newsy</div>
                 </div>
                 <div className="logo"></div>
             </div>
